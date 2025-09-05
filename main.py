93b4fb7e6809e813f56ba4bcebbc4a90e6e0f910
renny hong <rennyhong1103@gmail.con>
2025-09-04 21:02:49 -0700

    new month

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import csv
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 year = 2025
-month = 6
+month = 8
 
 def gmail_send_message(service, message):
   """Create and insert a draft email.
